6ab0f16239cf14b11415b876474f5c361ba274c9
Jermery Wheeler <jermery.wheeler@gmail.com>
2023-01-07 15:15:46 -0500

    adding routs to the index.js

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,12 @@ routes.get('/test', (req, res) => {
     res.send('Jermery Wheeler');
 });
 
+routes.get('/test2', (req, res) => {
+    res.send('Alexandra Wheeler');
+});
+
+routes.get('/test3', (req, res) => {
+    res.send('Chase Wheeler');
+});
+
 module.exports = routes; 
