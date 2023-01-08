80043e8b3196bc4ee3271b6b59393c188b023046
Jermery Wheeler <jermery.wheeler@gmail.com>
2023-01-08 05:02:34 -0500

    Commented out/ remamed files

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,9 @@ const ObjectId = require('mongodb').ObjectId;
 const getAll = async (req, res, next) => {
   const result = await mongodb.getDb().db("cse341").collection('contacts').find();
   result.toArray().then((lists) => {
+
     res.setHeader('Content-Type', 'application/json');
+    
     res.status(200).json(lists);
   });
 };
@@ -17,7 +19,9 @@ const getSingle = async (req, res, next) => {
     .collection('contacts')
     .find({ _id: userId });
   result.toArray().then((lists) => {
+
     res.setHeader('Content-Type', 'application/json');
+
     res.status(200).json(lists[0]);
   });
 };
