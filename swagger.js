9347fd5104b2332136227386c4cd0cdcb2a0269b
Jermery Wheeler <Jermery Wheeler>
2023-01-13 20:15:44 -0500

    working with localhost:8080

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: 'My Contacts API',
         description: 'Contacts API',
     },
-    host: 'cse341-898f.onrender.com',
-    schemes: ['https'],
+    host: 'localhost:8080',
+    schemes: ['http'],
 };
 
 const outputFile = './swagger.json';
